a94c978c282f93d3c5a7e38e3ea1997df06f9f55
snady <sfang2016@gmail.com>
2016-01-21 14:07:16 -0500

    spgfjkgjdf

--- server.c
+++ server.c
@@ -13,7 +13,20 @@ int main(){
 
   char buffer[256];
   int socket_id, socket_client;
+  int my_port = MY_PORT;
 
+  socket_id = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in listener;
+  listener.sin_family = AF_INET;     //socket type IPv4
+  listener.sin_port = htons(my_port);
+  listener.sin_addr.s_addr = INADDR_ANY;   //bind to any incoming address
+
+  bind(socket_id, (struct sockaddr*)&listener, sizeof(listener));
+
+  listen(socket_id, 1);
+  printf("<server> Listening\n");
+  
   /*
   int socket_id, socket_client;
 
@@ -29,20 +42,6 @@ int main(){
   */
 
   while(1){
-
-    //create socket
-    socket_id = socket(AF_INET, SOCK_STREAM, 0);
-
-    //bind to port
-    struct sockaddr_in listener;
-    listener.sin_family = AF_INET;     //socket type IPv4
-    listener.sin_port = htons(MY_PORT);
-    listener.sin_addr.s_addr = INADDR_ANY;   //bind to any incoming address
-    bind(socket_id, (struct sockaddr*)&listener, sizeof(listener));
-
-    listen(socket_id, 1);
-    printf("<server> Listening\n");
-
     socket_client = accept(socket_id, NULL, NULL);
     printf("<server> Connected: %d\n", socket_client);
 
@@ -50,11 +49,13 @@ int main(){
         
     if( chpid == 0 ) {
       while( read(socket_client, buffer, 255) ){
+        close(socket_id);
         write(socket_client, "Spaghetti Received", 255);
         printf("<server> Recieved %s\n",buffer);
-        break;
       }
+      break;
     }else{
+      close(socket_client);
       continue;
     }
   }
